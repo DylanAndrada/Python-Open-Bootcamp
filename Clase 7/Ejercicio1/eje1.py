12af204e8cc32a3af3769d0438444a25bf24bcfc
DylanAndrada <dylan.andrada192276@alumnos.info.unlp.edu.ar>
2023-03-07 15:48:13 -0300

    eje7

---
+++ Clase 7/Ejercicio1/eje1.py
@@ -0,0 +1,13 @@
+import operaciones as op
+
+a = 10
+
+b = 5
+
+print("Resta:",op.restar(a,b))
+
+print("Suma:",op.sumar(a,b))
+
+print("Multiplicacion:",op.multiplicar(a,b))
+
+print("Division:",op.dividir(a,b))
